ea08bf63989f6ab0f9f38b272f191ab0d8575010
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-08-02 18:59:02 +0800

    Delete scrips.js

--- scrips.js
+++
@@ -1,9 +0,0 @@
-// Нээх
-document.getElementById("menu-program").addEventListener("click", function () {
-  document.getElementById("programModal").style.display = "block";
-});
-
-// Хаах
-document.getElementById("closeProgram").addEventListener("click", function () {
-  document.getElementById("programModal").style.display = "none";
-});
