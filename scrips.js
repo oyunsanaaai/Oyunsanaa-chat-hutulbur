6c89abd346c28b465bf1d162acfc6e583766bf66
oyunsanaaai <oyunsanaa.ai@gmail.com>
2025-08-02 18:57:13 +0800

    Create scrips.js

---
+++ scrips.js
@@ -0,0 +1,9 @@
+// Нээх
+document.getElementById("menu-program").addEventListener("click", function () {
+  document.getElementById("programModal").style.display = "block";
+});
+
+// Хаах
+document.getElementById("closeProgram").addEventListener("click", function () {
+  document.getElementById("programModal").style.display = "none";
+});
